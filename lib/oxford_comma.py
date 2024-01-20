5ff800a24f6337e72c213035f87538660a8c40cd
Winnie <wimacoj@gmail.com>
2024-01-20 18:59:19 +0300

    Completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
+# def oxford_comma(items):
+#     # return ' and '.join(items)
+#     return ' and '.join([', '.join(items[:-1]), items[-1]] if len(items) > 2 else items)
+
+
 def oxford_comma(items):
-    return None
+    if len(items) > 2:
+        return ', '.join(items[:-1]) + ', and ' + items[-1]
+    elif len(items) == 2:
+        return ' and '.join(items)
+    elif len(items) == 1:
+        return items[0]
+    else:
+        return ''
